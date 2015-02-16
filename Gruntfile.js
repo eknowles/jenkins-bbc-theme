a2f1f93eea5fed728cc85e12a7556fc574e58e1e
Edward Knowles <mail@eknowles.com>
2015-02-16 12:51:48 +0000

    add package

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,20 +8,31 @@ module.exports = function (grunt) {
         '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
         '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
         ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+        usebanner: {
+            dist: {
+                options: {
+                    position: 'top',
+                    banner: '<%= banner %>'
+                },
+                files: {
+                    src: [ 'build/latest.css' ]
+                }
+            }
+        },
         less: {
             development: {
                 options: {
-                    paths: ["styles"]
+                    paths: ['styles']
                 },
                 files: {
-                    "build/latest.css": "main.less"
+                    'build/latest.css': 'main.less'
                 }
             },
             production: {
                 options: {
-                    paths: ["styles"],
+                    paths: ['styles'],
                     plugins: [
-                        new (require('less-plugin-autoprefix'))({browsers: ["last 2 versions"]}),
+                        new (require('less-plugin-autoprefix'))({browsers: ['last 2 versions']}),
                         new (require('less-plugin-clean-css'))({advanced:true, compatibility: 'ie8'})
                     ],
                     modifyVars: {
@@ -30,18 +41,16 @@ module.exports = function (grunt) {
                     }
                 },
                 files: {
-                    "build/latest.css": "main.less"
+                    'build/latest.css': 'main.less'
                 }
             }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('less-plugin-autoprefix');
-    grunt.loadNpmTasks('less-plugin-clean-css');
+    grunt.loadNpmTasks('grunt-banner');
 
-
-
-    grunt.registerTask('default', ['less']);
+    grunt.registerTask('default', ['less:development']);
+    grunt.registerTask('build', ['less:production', 'usebanner:dist']);
 
 };
